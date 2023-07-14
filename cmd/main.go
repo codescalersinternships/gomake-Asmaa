2ad0dc37310c27d5d1abb95c3b351e023ce30a83
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-07-14 13:28:10 +0300

    update project

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -14,7 +13,7 @@ func main() {
 		fmt.Println("Error parsing Makefile:", err)
 		return
 	}
-	
+
 	file, err := internal.CheckMakeFile(filePath)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -39,46 +38,3 @@ func main() {
 		return
 	}
 }
-
-
-package main
-
-import (
-	"fmt"
-
-	"github.com/codescalersinternships/gomake-Asmaa/internal"
-)
-
-func main() {
-
-	filePath, target, err := internal.ParseCommand()
-	if err != nil {
-		fmt.Println("Error parsing Makefile:", err)
-		return
-	}
-	
-	file, err := internal.CheckMakeFile(filePath)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	graph, err := internal.ParseMakefile(file)
-	if err != nil {
-		fmt.Println("Error parsing Makefile:", err)
-		return
-	}
-
-	err = internal.CheckCircularDependencies(graph)
-	if err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-
-	err = internal.RunTarget(graph, target)
-	if err != nil {
-		fmt.Println("Error running target:", err)
-		return
-	}
-}
-
